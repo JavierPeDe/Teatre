85dead1217091d14d365240f870f3ae974517fd1
javierPerez <javip107@gmail.com>
2021-05-19 11:13:55 +0200

    Implementation of the Line component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Linia from './components/Linia';
+import obraTeatral from './data/obraTeatral.json';
 
 function App() {
+  
+  const frases = obraTeatral.map((frase) => {
+    return <Linia key={frase} info={frase} />;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Nivel -1</h1>
+      {frases}
     </div>
   );
 }
---
+++ src/components/Linia.js
@@ -0,0 +1,6 @@
+//Muestra por pantalla el texto que entre por info.
+
+const Linia = (props) => {
+  return <p>{props.info}</p>;
+};
+export default Linia;
